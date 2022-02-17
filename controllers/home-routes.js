32ce2ec4ede13ccb551ffb4ab2a74b2a32688016
sarahwylie <sarahannemariawylie@gmail.com>
2022-02-17 16:59:19 -0600

    setting up routes and models

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
