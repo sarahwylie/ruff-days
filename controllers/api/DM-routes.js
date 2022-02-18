923a73a5320a9c51a29bde0c4ca88e4ae18f140f
Travis Bryant <tbryant2@berklee.edu>
2022-02-17 20:10:59 -0600

    class 2

---
+++ controllers/api/DM-routes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth')
+const { DM } = require('../../models');
+
+router.get('/', (req, res) => {
+    DM.findAll()
+      .then(dbDMData => res.json(dbDMData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+    DM.create({
+        DMt_text: req.body.DM_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbDMData => res.json(dbDMData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+}});
+
+router.delete('/:id', withAuth, (req, res) => {
+    DM.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbDMData => {
+            if (!dbDMData) {
+                res.status(404).json({ message: 'No DM found with this id ' });
+                return;
+            }
+            res.json(dbDMData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+module.exports = router;
