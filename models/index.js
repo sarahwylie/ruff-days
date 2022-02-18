78393e0ba243a7964e253d14a0f717539671b7aa
sarahwylie <sarahannemariawylie@gmail.com>
2022-02-17 20:42:17 -0600

    we have ONE functional page!

--- models/index.js
+++ models/index.js
@@ -1,3 +1,28 @@
 const User = require('./User');
+const Post = require('./Post');
+const DM = require('./DM');
 
-module.exports = { User };
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+DM.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+DM.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(DM, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(DM, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, DM };
