4a5ce717634d5f2fb6067c588b1507019c4d13c8
sarahwylie <sarahannemariawylie@gmail.com>
2022-02-26 12:31:48 -0600

    signup route fixed

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,31 +1,31 @@
 const { Post } = require('../models');
 
 const postdata = [
-    // {
-    //     dogname: 'Today was Monday',
-    //     breed: 'https://mondaysammirite.com',
-    //     user_id: 8
-    // },
-    // {
-    //     dogname: 'It is Tuesday',
-    //     breed: 'https://buzzfeed.com/tuesdays',
-    //     user_id: 4
-    // },
-    // {
-    //     dogname: 'Wednesday is in the middle',
-    //     breed: 'https://www.middle.com',
-    //     user_id: 4
-    // },
-    // {
-    //     dogname: 'Donec posuere metus vitae ipsum.',
-    //     breed: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
-    //     user_id: 10
-    // },
-    // {
-    //     dogname: 'Donec posuere metus vitae ipsum.',
-    //     breed: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
-    //     user_id: 10
-    // },
+    {
+        dogname: 'Mochi',
+        breed: 'husky',
+        user_id: 8
+    },
+    {
+        dogname: 'Moo',
+        breed: 'staffie',
+        user_id: 4
+    },
+    {
+        dogname: 'Chico',
+        breed: 'chihuahua',
+        user_id: 4
+    },
+    {
+        dogname: 'Poppi',
+        breed: 'mutt',
+        user_id: 10
+    },
+    {
+        dogname: 'Hank',
+        breed: 'boston terrorist',
+        user_id: 10
+    },
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
